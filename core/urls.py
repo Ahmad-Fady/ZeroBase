4f35546fe86d726ede26e4e652b6f98a138d312a
AhmadFady <AhmadFady.dev@proton.me>
2026-04-27 14:33:46 +0300

    added curser ui static files

--- core/urls.py
+++ core/urls.py
@@ -15,11 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
-from django.contrib import admin
 from django.urls import path
 from . import views
 
+app_name = "core"
 urlpatterns = [
-    path("admin/", admin.site.urls),
     path("", views.index, name="home"),
+    path("internships", views.internships, name="internships"),
+    path("jobs", views.jobs, name="jobs"),
+    path("learn", views.learn, name="learn"),
 ]
--- core/views.py
+++ core/views.py
@@ -5,3 +5,15 @@ from .models import *
 def index(request):
     """The home page."""
     return render(request, "core/index.html")
+
+def internships(request):
+    """The home page."""
+    return render(request, "core/internships.html")
+
+def jobs(request):
+    """The home page."""
+    return render(request, "core/jobs.html")
+
+def learn(request):
+    """The home page."""
+    return render(request, "core/learn.html")
